d6c686932f938a8abfec8752c352b9ba1c037959
Luketto-dev <luca.francesconi1995@gmail.com>
2022-12-14 15:41:13 +0100

    client-side-validation form

---
+++ pages/index.jsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+
+export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data.name);
+  };
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          {...register("name", { required: true, minLength: 2 })}
+        />
+        {errors.name && <p>name is invalid</p>}
+        <input
+          type="text"
+          placeholder="cognome"
+          name="surname"
+          {...register("surname", {
+            required: "surname required",
+            minLength: {
+              value: 8,
+              message: "il cognome deve avere lunghezza minima di 8",
+            },
+            maxLength:{
+              value: 12,
+              message: "il cognome deve avere lunghezza massima di 12",
+            }
+          })}
+        />
+        {errors.surname && <p>{errors.surname.message}</p>}
+        <button>invia</button>
+      </form>
+    </div>
+  );
+}
